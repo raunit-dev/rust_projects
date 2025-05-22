7ab47d14f363793433041ff53fdfc49922787ed5
solanagithubraunit15 <raunitjaiswal510@gmail.com>
2025-05-22 11:00:56 +0000

    enums,optionsenumbs,resultenums

--- Practise/src/main.rs
+++ Practise/src/main.rs
@@ -1,42 +1,42 @@
+use std::fs;
+
 trait Shape {
-    fn area(&self) -> f64; 
+    fn area(&self) -> f64;
     fn perimeter(&self) -> f64;
 }
 
 struct Square {
-   length: f64
+    length: f64,
 }
 
 impl Shape for Square {
     fn area(&self) -> f64 {
-        return  self.length  * self.length;
+        self.length * self.length
     }
 
     fn perimeter(&self) -> f64 {
-        return 4.0 * self.length;
+        4.0 * self.length
     }
 }
 
 struct Rect {
     height: f32,
-    width: f32
+    width: f32,
 }
 
 impl Rect {
     fn area(&self) -> f32 {
-        return self.width * self.height;
+        self.width * self.height
     }
 
     fn perimeter(&self) -> f32 {
-        return 2.0 * (self.width + self.height);
+        2.0 * (self.width + self.height)
     }
 
     fn print_something(_a: u32) {
         println!("Static Function");
     }
 }
-
-//match.enums,struct damnnnnn
 
 #[derive(Debug)]
 enum Direction {
@@ -46,54 +46,86 @@ enum Direction {
     West,
 }
 
-struct DirectionType {
-    direction : Direction
+enum Shapes {
+    Square(f32),
+    Circle(f32),
+    Rectangle(f32, f32),
 }
 
+struct DirectionType {
+    direction: Direction,
+}
 
+fn calculate_area(s: Shapes) -> f32 {
+    match s {
+        Shapes::Square(side) => side * side,
+        Shapes::Circle(radius) => std::f32::consts::PI * radius * radius,
+        Shapes::Rectangle(width, height) => width * height,
+    }
+}
 
-fn main () {
+fn main() {
     let r = Rect {
         width: 10.0,
-        height: 10.0
+        height: 10.0,
     };
 
-    let Square = Square {
-        length: 2.0
-    };
+    let square = Square { length: 2.0 };
 
     let dir = DirectionType {
-        direction: Direction::East
+        direction: Direction::East,
     };
- 
+
     let answer = match dir.direction {
-        Direction::East => {
-            true
-        }
-        ,
-        Direction::West => {
-            true
-        },
-        Direction::North => {
-            true
-        },
-        Direction::South => {
-            true
-        }
-    }; 
+        Direction::East => true,
+        Direction::West => true,
+        Direction::North => true,
+        Direction::South => true,
+    };
 
-    println!("{}",answer);
+    println!("{}", answer);
 
-    
+    let shape = Shapes::Square(10.0);
+    let shape_circle = Shapes::Circle(10.0);
+    let shape_rect = Shapes::Rectangle(10.0, 10.0);
+
+    println!("{}", calculate_area(shape));
+    println!("{}", calculate_area(shape_circle));
+    println!("{}", calculate_area(shape_rect));
 
     let my_direction = Direction::North;
     println!("{:?}", my_direction);
 
+    let day2 = "Day2";
+    println!("{}", day2);
+
     println!("{}, {}", r.width, r.height);
     println!("{}", r.area());
     println!("{}", r.perimeter());
-    println!("{}", Square.length);
-    println!("{}", Square.area());
-    println!("{}", Square.perimeter());
+    println!("{}", square.length);
+    println!("{}", square.area());
+    println!("{}", square.perimeter());
     Rect::print_something(10);
+
+
+    let contents =  fs::read_to_string("a.txt");
+
+    match contents {
+        Ok(contents ) => println!("{}",contents),
+        Err(_) => println!("Error while reading the file")
+    }
+
+    let write_result = fs::write("a.txt", "Hey i am writing into a file");
+
+    match write_result {
+        Ok(_) => {
+            println!("Successfully wrote to the file.");
+            // Read back the contents to print what was written
+            match fs::read_to_string("a.txt") {
+                Ok(contents) => println!("File contents: {}", contents),
+                Err(_) => println!("Error while reading the file"),
+            }
+        }
+        Err(_) => println!("Error while writing the file"),
+    }
 }
