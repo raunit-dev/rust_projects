89bab060988dd1c2a4c2477e159bf1ea46aff8e4
solanagithubraunit15 <raunitjaiswal510@gmail.com>
2025-05-26 19:46:23 +0000

    done with few rust concepts

---
+++ Practise/src/incdec.rs
@@ -0,0 +1,48 @@
+use std::fs;
+
+use borsh::{BorshDeserialize, BorshSerialize};
+
+#[derive(BorshDeserialize, BorshSerialize, Debug, PartialEq)]
+struct Counter {
+    number: u8,
+}
+  
+impl Counter {
+    fn increment(&mut self) {
+        self.number = self.number.saturating_add(1);
+    }
+
+    fn decrement(&mut self) {
+        self.number = self.number.saturating_sub(1);
+    }
+}
+
+fn main() {
+    let mut c = Counter { number: 0 };
+
+    // Serialize and write to file
+    let mut buffer: Vec<u8> = Vec::new();
+    c.serialize(&mut buffer).unwrap();
+    println!("{:?}", buffer);
+    let write_result = fs::write("no.txt", &buffer);
+    match write_result {
+        Ok(_) => println!("Successfully wrote to the file."),
+        Err(_) => println!("Error"),
+    }
+
+    // Read from file and deserialize
+    let c_read = fs::read("no.txt");
+    match c_read {
+        Ok(contents) => {
+            let deserialized = Counter::try_from_slice(&contents).unwrap();
+            println!("File contents: {:?}", deserialized);
+        }
+        Err(_) => println!("Error while reading the file"),
+    }
+
+    // Example usage of increment and decrement
+    c.increment();
+    println!("After increment: {:?}", c);
+    c.decrement();
+    println!("After decrement: {:?}", c);
+}
--- Practise/src/main.rs
+++ Practise/src/main.rs
@@ -1,48 +1,18 @@
-use std::fs;
-
-use borsh::{BorshDeserialize, BorshSerialize};
-
-#[derive(BorshDeserialize, BorshSerialize, Debug, PartialEq)]
-struct Counter {
-    number: u8,
-}
-
-impl Counter {
-    fn increment(&mut self) {
-        self.number = self.number.saturating_add(1);
-    }
-
-    fn decrement(&mut self) {
-        self.number = self.number.saturating_sub(1);
-    }
-}
-
 fn main() {
-    let mut c = Counter { number: 0 };
+    let str1 = String::from("Raunit");
+    let ans;
+    {
+        let str2 = String::from("jaiswalistheman");
+        ans = string_longest(&str1,&str2);
+        println!("{}",ans);
+    }
+    
+}
 
-    // Serialize and write to file
-    let mut buffer: Vec<u8> = Vec::new();
-    c.serialize(&mut buffer).unwrap();
-    println!("{:?}", buffer);
-    let write_result = fs::write("no.txt", &buffer);
-    match write_result {
-        Ok(_) => println!("Successfully wrote to the file."),
-        Err(_) => println!("Error"),
+fn string_longest<'a>(s1: &'a String, s2: &'a String) -> &'a String {
+    if s1.len() > s2.len() {
+        return s1;
     }
 
-    // Read from file and deserialize
-    let c_read = fs::read("no.txt");
-    match c_read {
-        Ok(contents) => {
-            let deserialized = Counter::try_from_slice(&contents).unwrap();
-            println!("File contents: {:?}", deserialized);
-        }
-        Err(_) => println!("Error while reading the file"),
-    }
-
-    // Example usage of increment and decrement
-    c.increment();
-    println!("After increment: {:?}", c);
-    c.decrement();
-    println!("After decrement: {:?}", c);
+    return s2;
 }
